3632ac06e98b117cf47d30b2cb1fc8982b6bbe07
devborgis <borgesv770@gmail.com>
2024-03-26 18:38:31 -0300

    add: DataModule para controle da aplicação e banco de dados SQLite3

---
+++ dmSystem.dfm
@@ -0,0 +1,76 @@
+﻿object mSystem: TmSystem
+  OnCreate = DataModuleCreate
+  Height = 328
+  Width = 388
+  object conSystem: TFDConnection
+    Params.Strings = (
+      'Database=C:\Borgis\borgis.db'
+      'DriverID=SQLite')
+    Connected = True
+    LoginPrompt = False
+    Left = 56
+    Top = 40
+  end
+  object qryLogin: TFDQuery
+    Active = True
+    Connection = conSystem
+    SQL.Strings = (
+      'select * from tb_users')
+    Left = 128
+    Top = 40
+  end
+  object qryUsers: TFDQuery
+    Active = True
+    Connection = conSystem
+    SQL.Strings = (
+      'SELECT '
+      'ID, '
+      'NAME AS '#39'NOME'#39
+      'FROM TB_USERS'
+      '')
+    Left = 56
+    Top = 104
+  end
+  object qryReports: TFDQuery
+    Active = True
+    Connection = conSystem
+    SQL.Strings = (
+      'SELECT '
+      'R.ID, '
+      'R.DESCRIPTION AS '#39'DESCRI'#199#195'O'#39','
+      'RG.DESCRIPTION AS '#39'GRUPO'#39
+      'FROM TB_REPORTS AS R'
+      'LEFT JOIN TB_REPORTS_GROUP AS RG ON RG.ID = R.ID_GROUP')
+    Left = 120
+    Top = 104
+    object qryReportsID: TIntegerField
+      Alignment = taCenter
+      FieldName = 'ID'
+      Origin = 'ID'
+      ProviderFlags = [pfInUpdate, pfInWhere, pfInKey]
+    end
+    object qryReportsDESCRIÇÃO: TStringField
+      FieldName = 'DESCRI'#199#195'O'
+      Origin = 'DESCRIPTION'
+      Size = 100
+    end
+    object qryReportsGRUPO: TStringField
+      AutoGenerateValue = arDefault
+      FieldName = 'GRUPO'
+      Origin = 'DESCRIPTION'
+      ProviderFlags = []
+      ReadOnly = True
+      Size = 60
+    end
+  end
+  object dsUsers: TDataSource
+    DataSet = qryUsers
+    Left = 56
+    Top = 168
+  end
+  object dsReports: TDataSource
+    DataSet = qryReports
+    Left = 120
+    Top = 168
+  end
+end
---
+++ dmSystem.pas
@@ -0,0 +1,68 @@
+{*****************************************************************************
+ * Nome do Arquivo: dmSystem.pas
+ * Descrição: DataModule para controle interno do sistema como versão, usuarios e armazenagem de relatorios
+ * Licença: Apache License, Version 2.0
+ ******************************************************************************
+ * Copyright (c) 2024 Vinicius Borges Gomes - Borgis & Co. Desenvolvimento
+ ******************************************************************************
+ * Informações de Licença:
+ *
+ * Este software é licenciado sob os termos da Licença Apache, Versão 2.0 (a "Licença");
+ * Você não pode usar este arquivo, exceto em conformidade com a Licença.
+ * Você pode obter uma cópia da Licença em
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * A menos que exigido por lei aplicável ou acordado por escrito, software
+ * distribuído sob a Licença é distribuído "COMO ESTÁ", SEM GARANTIAS OU CONDIÇÕES DE QUALQUER TIPO,
+ * expressa ou implícita.
+ * Consulte a Licença para as permissões específicas que regem o uso do software.
+ * ****************************************************************************}
+
+unit dmSystem;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, ZAbstractConnection, ZConnection, Data.DB,
+  ZAbstractRODataset, ZAbstractDataset, ZDataset, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.VCLUI.Wait, FireDAC.Comp.Client, FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs,
+  FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Comp.UI;
+
+type
+  TmSystem = class(TDataModule)
+    conSystem: TFDConnection;
+    qryLogin: TFDQuery;
+    qryUsers: TFDQuery;
+    qryReports: TFDQuery;
+    dsUsers: TDataSource;
+    dsReports: TDataSource;
+    qryReportsID: TIntegerField;
+    qryReportsDESCRIÇÃO: TStringField;
+    qryReportsGRUPO: TStringField;
+    procedure DataModuleCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  mSystem: TmSystem;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+procedure TmSystem.DataModuleCreate(Sender: TObject);
+begin
+  conSystem.Params.Database := ExtractFilePath(ParamStr(0)) + '.system\' + 'borgis.db';
+end;
+
+end.
