28cf65c3d6610aaee312068a34492769f3029d7e
devborgis <borgesv770@gmail.com>
2024-06-20 08:39:52 -0300

    alterações do delphi

--- rlsBorgis.dpr
+++ rlsBorgis.dpr
@@ -3,14 +3,15 @@ program rlsBorgis;
 uses
   Vcl.Forms,
   uLogin in 'forms\sistema\uLogin.pas' {frmLogin},
-  uConfig in 'forms\sistema\uConfig.pas' {frmConfig},
+  uconfig in 'forms\sistema\uconfig.pas' {frmConfig},
   uSystem in 'forms\sistema\uSystem.pas' {frmSystem},
   dmIntegracao in 'data\integracao\dmIntegracao.pas' {mIntegracao: TDataModule},
   dmSystem in 'data\sistema\dmSystem.pas' {mSystem: TDataModule},
   uCadUser in 'forms\cadastros\uCadUser.pas' {frmCadUser},
   uCadReport in 'forms\cadastros\uCadReport.pas' {frmCadReport},
   dmFastReport in 'data\fastreport\dmFastReport.pas' {mFastReport: TDataModule},
-  uCadGroupReport in 'forms\cadastros\uCadGroupReport.pas' {frmCadGroupReport};
+  uCadGroupReport in 'forms\cadastros\uCadGroupReport.pas' {frmCadGroupReport},
+  uUtils in 'uUtils.pas';
 
 {$R *.res}
 
