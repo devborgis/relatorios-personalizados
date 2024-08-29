163c2473b8abd0919a9ebfede1675779d2e14f3f
Vinicius Borges <110197392+devborgis@users.noreply.github.com>
2024-08-29 17:11:01 -0300

    refactor: mudança na criação de formulários
    Ajustada a aplicação para não criar automaticamente todos os formulários na inicialização. Agora, os formulários serão criados sob demanda, apenas quando o usuário os chamar. Isso melhora a eficiência e o gerenciamento de recursos da aplicação. (ainda no papel pensando em criar uma tela de loading assim o sistema ao ser iniciado não vai criar nada a não ser que atenda a pré requisitos)

--- rlsBorgis.dpr
+++ rlsBorgis.dpr
@@ -11,23 +11,27 @@ uses
   uCadReport in 'forms\cadastros\uCadReport.pas' {frmCadReport},
   dmFastReport in 'data\fastreport\dmFastReport.pas' {mFastReport: TDataModule},
   uCadGroupReport in 'forms\cadastros\uCadGroupReport.pas' {frmCadGroupReport},
-  uUtils in 'uUtils.pas',
-  uConf in 'uConf.pas';
+  uUtils in 'classes\uUtils.pas',
+  uConf in 'classes\uConf.pas';
 
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.Title := 'Borgis - Relatórios Personalizados';
+  Application.CreateForm(TmSystem, mSystem);
+  Application.CreateForm(TmIntegracao, mIntegracao);
+  Application.CreateForm(TmFastReport, mFastReport);
   Application.CreateForm(TfrmLogin, frmLogin);
+  {a aplicação não vai mais criar os formularios serao criados
+  de acordo com o uso
+
   Application.CreateForm(TfrmConfig, frmConfig);
   Application.CreateForm(TfrmSystem, frmSystem);
-  Application.CreateForm(TmIntegracao, mIntegracao);
-  Application.CreateForm(TmSystem, mSystem);
   Application.CreateForm(TfrmCadUser, frmCadUser);
   Application.CreateForm(TfrmCadReport, frmCadReport);
-  Application.CreateForm(TmFastReport, mFastReport);
-  Application.CreateForm(TfrmCadGroupReport, frmCadGroupReport);
+  Application.CreateForm(TfrmCadGroupReport, frmCadGroupReport);}
   Application.Run;
 end.
