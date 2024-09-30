091e3794c13efc7fefe8fb5a99d9afa41a75d696
Vinicius Borges <borgesv770@gmail.com>
2024-09-30 19:34:51 -0300

    tests e alterações do próprio delphi

--- rlsBorgis.dpr
+++ rlsBorgis.dpr
@@ -12,7 +12,13 @@ uses
   dmFastReport in 'data\fastreport\dmFastReport.pas' {mFastReport: TDataModule},
   uCadGroupReport in 'forms\cadastros\uCadGroupReport.pas' {frmCadGroupReport},
   uUtils in 'classes\uUtils.pas',
-  uConf in 'classes\uConf.pas';
+  uConf in 'classes\uConf.pas',
+  uCarregando in 'forms\sistema\uCarregando.pas' {frmCarregando},
+  Vcl.Themes,
+  Vcl.Styles,
+  uConfiguracoes in 'forms\sistema\uConfiguracoes.pas' {frmConfiguracoes},
+  uListaRelatorio in 'forms\sistema\uListaRelatorio.pas' {frmListaRelatorio},
+  uListaUsuarios in 'forms\sistema\uListaUsuarios.pas' {frmListaUsuario};
 
 {$R *.res}
 
@@ -20,18 +26,27 @@ begin
   ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows11 Dark');
   Application.Title := 'Borgis - Relatórios Personalizados';
   Application.CreateForm(TmSystem, mSystem);
   Application.CreateForm(TmIntegracao, mIntegracao);
+  //Application.CreateForm(TfrmListaUsuario, frmListaUsuario);
+  //Application.CreateForm(TfrmListaRelatorio, frmListaRelatorio);
+  //Application.CreateForm(TmIntegracao, mIntegracao);
   Application.CreateForm(TmFastReport, mFastReport);
-  Application.CreateForm(TfrmLogin, frmLogin);
+  //Application.CreateForm(TfrmLogin, frmLogin);
+  //Application.CreateForm(TfrmCarregando, frmCarregando);
   {a aplicação não vai mais criar os formularios serao criados
   de acordo com o uso
 
   Application.CreateForm(TfrmConfig, frmConfig);
-  Application.CreateForm(TfrmSystem, frmSystem);
-  Application.CreateForm(TfrmCadUser, frmCadUser);
+
+
   Application.CreateForm(TfrmCadReport, frmCadReport);
   Application.CreateForm(TfrmCadGroupReport, frmCadGroupReport);}
+  //Application.CreateForm(TfrmCadUser, frmCadUser);
+  //Application.CreateForm(TfrmSystem, frmSystem);
+  Application.CreateForm(TfrmLogin, frmLogin);
+  //Application.CreateForm(TfrmConfiguracoes, frmConfiguracoes);
   Application.Run;
 end.
